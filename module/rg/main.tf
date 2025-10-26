ee0c553230e123c5eb4842dd180723a1d17b6200
mahendra kumar <kumar.mahendra1712@gmail.com>
2025-10-26 14:51:31 +0530

    second_time

--- module/rg/main.tf
+++ module/rg/main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "mahi_01" {
   name     = "rg-mahi"
   location = "central india"
 }
+resource "azurerm_resource_group" "mahi_02" {
+  name     = "rg-mah"
+  location = "central india"
+}
